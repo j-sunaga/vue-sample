d8a1271bcc7cb5087128503f5633d1f9fde2c497
j-sunaga <124577blue@gmail.com>
2020-04-20 12:12:30 +0900

    追加したデータを表示できるように機能を追加

--- sample.js
+++ sample.js
@@ -18,6 +18,7 @@ new Vue({
     addStudent: function () {
       // この function() { } の中にaddStudentメソッド
       // （入力欄に入力された値と、ID値（最後に登録されたID値+1の値）が新たに出現する機能）の処理を書く
+      this.students.push({ id: this.students.length + 1, name: this.name, course: this.course, acceptancePeriod: this.acceptancePeriod });
     }
   }
 })
